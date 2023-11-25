b4fe359e8ce912e04ffc006330aa6779f057cebb
minhajp4323 <minhajp4323@gmail.com>
2023-11-25 14:53:35 +0530

    Props Updation

--- hooks react/hooks/src/App.js
+++ hooks react/hooks/src/App.js
@@ -2,13 +2,34 @@ import React from 'react';
 import './App.css';
 import StateTutorial from './components/useState';
 import { Tutorial } from './components/useState';
+import Products from './components/props';
 
 function App() {
   return (
     <>
+      <h2>UseStates</h2>
       <StateTutorial />
+  
+
       <br></br>
       <Tutorial />
+      <h1>Props</h1>
+      <Products 
+        name= 'Amazone echo' 
+        description = 'your AI Assistance' 
+        price= {59.99}
+     />
+     <Products 
+        name= '15 Pro Max' 
+        description = 'The best iPhone everr' 
+        price= {1599}
+     />
+     <Products 
+        name= 'MacBook Pro' 
+        description = 'your Favourite computer' 
+        price= {2599}
+     />
+  
 
     </>
   );
--- hooks react/hooks/src/components/useState.js
+++ hooks react/hooks/src/components/useState.js
@@ -6,19 +6,29 @@ const StateTutorial = ()=>{
   let increment=()=>{
     
     setCounter(counter+1)
+
+  }
+  let decrement=()=>{
+    setCounter(counter-1)
   }
   return(
-  <div className="main">{counter}
-  <button onClick={increment}>Increment</button> </div>
+  <div className="main">{counter}<br></br>
+  <button onClick={increment}>Increment</button> <br>
+</br>
+  <button onClick={decrement}>decrement</button> </div>
   )
 };
 
 export default StateTutorial
 
+
+
+
+
 // useState 2nd example
 
 export const Tutorial =()=>{
-  const [inputValue, setInputValue]= useState("Minhaj");
+  const [inputValue, setInputValue]= useState(" ");
   let onChange = (event)=>{
     const newValue= event.target.value;
     setInputValue(newValue)
