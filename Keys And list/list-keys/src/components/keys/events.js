c57bc7a7778bddeef177c50ed65fef57aeb5d3f4
minhajp4323 <minhajp4323@gmail.com>
2023-11-27 15:31:20 +0530

    events  completed

--- Keys And list/list-keys/src/components/keys/events.js
+++ Keys And list/list-keys/src/components/keys/events.js
@@ -7,12 +7,27 @@ const Header= ()=>{
     const dbclickHandle=(name)=>{
         alert("hello " + name)
     };
+    const onChangeHandle=()=>{
+        console.log("you typed in the text field")
+        
+    };
+    // const onMouseOverHandle=()=>{
+    //     alert("you mouse over on the element")
+        
+    // };
+    const onKeyDownHandle=()=>{
+        alert("you pressed a key")
+    }
     return(
         <>
         <h1>Learn React Events</h1>
         <button onClick={clickHandle} onDoubleClick={dbclickHandle}>Click Me</button>
-        <h1 onDoubleClick={()=>dbclickHandle("rahul")}>Double Click</h1>
-        <input type="text" />
+        <h1 onDoubleClick={()=>dbclickHandle("Jishmal")}>Double Click</h1>
+        <input onChange={onChangeHandle} type="text" />
+        {/* <h1 onMouseOver={onMouseOverHandle}>Mouse Over</h1> */}
+        <input onKeyDown={onKeyDownHandle} />
+        
+
         </>
     )
 
