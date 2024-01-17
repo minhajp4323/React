1e0d0847f02aa01c70a465bd19ea3b411e3a3f6a
minhajp4323 <minhajp4323@gmail.com>
2024-01-17 14:50:23 +0530

    Todo Redux

---
+++ Redux/Todo/redux-todo/src/Todo/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import Reducer from "./Reducer";
+
+
+const store = configureStore({
+    reducer:{
+        todo: Reducer,
+    }
+})
+export default store;
