f55ae8cbe551f433680815ce2f5a931c98b6b4ca
minhajp4323 <minhajp4323@gmail.com>
2024-02-16 15:26:02 +0530

    fixes

--- Badge/badge/src/Componets/ColorChange.js
+++ Badge/badge/src/Componets/ColorChange.js
@@ -5,7 +5,7 @@ function ColorChange() {
   return (
     <div style={{backgroundColor: count===4?"red": "green"}}>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Click</button>
+      <button onClick={() => setCount(count + 1)}>Click.</button>..
     </div>
   );
 }
