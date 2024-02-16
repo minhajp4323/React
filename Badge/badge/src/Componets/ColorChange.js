0f993ec6c27796e8f82fb5951090aa0123f0185f
minhajp4323 <minhajp4323@gmail.com>
2024-02-16 17:31:14 +0530

    updations

--- Badge/badge/src/Componets/ColorChange.js
+++ Badge/badge/src/Componets/ColorChange.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function ColorChange() {
   const [count, setCount] = useState(0);
   return (
-    <div style={{backgroundColor: count===4?"red": "green"}}>
+    <div style={{backgroundColor: count===4?"blue": "green"}}>
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>Click.</button>..
     </div>
