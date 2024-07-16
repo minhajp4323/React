6f0d64b1956dd480c4f8d57891ec2c28c30a9487
minhajp4323 <minhajp4323@gmail.com>
2024-07-16 12:56:15 +0530

    Refreshing react

--- Badge/badge/src/Componets/ColorChange.js
+++ Badge/badge/src/Componets/ColorChange.js
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
+const profile = [
+  {
+    name: "Minhaj",
+    age: 22,
+    job: "developer",
+  },
+  { name: "Salim", age: 22, job: "jobless" },
+];
 function ColorChange() {
   const [count, setCount] = useState(0);
+  const backgroundColor = useMemo(() => {
+    return count % 3 === 0 ? "blue" : "red";
+  }, [count]);
   return (
-    <div style={{backgroundColor: count===4?"blue": "green"}}>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Click.</button>..
-    </div>
+    <>
+      <div style={{ backgroundColor }}>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>Click</button>
+      </div>
+
+    
+    </>
   );
 }
 
