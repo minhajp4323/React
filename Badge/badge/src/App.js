aa4573f52c2e37ae6905a4e7761efc3c07de868b
minhajp4323 <minhajp4323@gmail.com>
2024-07-30 15:37:23 +0530

    .

--- Badge/badge/src/App.js
+++ Badge/badge/src/App.js
@@ -1,11 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
-import ColorChange from "./Componets/ColorChange";
 
 function App() {
   return (
     <div className="App">
-      <ColorChange />
     </div>
   );
 }
---
+++ Badge/badge/src/Componets/TakeOff.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TakeOff() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TakeOff
